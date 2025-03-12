3040a0d5b71b9d4016e76d705ff830690bc27978
Titipong Pakinsri <titipong@thains.co.th>
2025-03-12 14:54:55 +0700

    commint

--- gpr-cli/run.sh
+++ gpr-cli/run.sh
@@ -1,3 +1,2 @@
-docker run --rm -d -h grp1-cli --name=grp1-cli --net=wsnet --ip=192.168.1.2 --dns=203.159.77.77 ubuntu
+docker run --rm -d -h grp1-cli --name=grp1-cli --net=wsnet --ip=192.168.1.2 --dns=203.159.77.77 thnicf/gprx-cli
 docker exec -it grp1-cli /bin/bash
-
--- gpr-resolve/run.sh
+++ gpr-resolve/run.sh
@@ -1,2 +1,2 @@
-docker run --rm -d -h grp1-resolv1 --name=grp1-resolv1 --net=wsnet --ip=192.168.1.53 --dns=203.159.77.77 -e TZ=UTC -p 32053:53 -p 32053:53/udp ubuntu/bind9:9.18-22.04_beta
+docker run --rm -d -h grp1-resolv1 --name=grp1-resolv1 --net=wsnet --ip=192.168.1.53 --dns=203.159.77.77 -e TZ=UTC -p 32053:53 -p 32053:53/udp thnicf/gprx-resolve
 docker exec -it grp1-resolv1 /bin/bash
