22a5d6bb76b379c5af6e51f2cbabe4d964d5ebf3
Titipong Pakinsri <titipong@thains.co.th>
2025-03-12 14:34:48 +0700

    commint

--- gpr-cli/run.sh
+++ gpr-cli/run.sh
@@ -1,3 +1,3 @@
-docker run --rm -d -h grp1-cli --name=grp1-cli --net=wsnet --ip=10.0.0.2 --dns=203.159.70.209 --dns=203.159.77.77 ubuntu
+docker run --rm -d -h grp1-cli --name=grp1-cli --net=wsnet --ip=192.168.1.2 --dns=203.159.77.77 ubuntu
 docker exec -it grp1-cli /bin/bash
 
